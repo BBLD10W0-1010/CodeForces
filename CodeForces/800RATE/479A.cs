ea7bdb6268c1b45a8ec7ab02434cb0edccb74fb1
BBLD10W0 <maksim.buchelnikov@urfu.me>
2026-04-05 22:16:46 +0500

    Добавьте файлы проекта.

---
+++ CodeForces/800RATE/479A.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeForces
+{
+    internal class _479A
+    {
+        public static void Solution()
+        {
+            var input = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToList();
+            var n = input[0];
+            var k = input[1];
+            for (int i = 0; i < k; i++)
+            {
+                if (n % 10 == 0)
+                {
+                    n = n / 10;
+                }
+                else
+                {
+                    n -= 1;
+                }
+            }
+            Console.WriteLine(n);
+        }
+    }
+}
---
+++ CodeForces/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeForces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            _479A.Solution();
+        }
+    }
+}
